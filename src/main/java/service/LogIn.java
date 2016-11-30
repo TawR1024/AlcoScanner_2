759c866ab8def729033ac020bc1359ed59a5df53
Ilya Kulakov <ilya.kulakov.4308@gmail.com>
2016-11-30 20:33:41 +0300

    init

---
+++ src/main/java/service/LogIn.java
@@ -0,0 +1,30 @@
+package service;
+
+import org.jetbrains.annotations.Contract;
+
+import java.util.HashMap;
+
+/**
+ * Created by ilya-kulakov on 30.11.16.
+ */
+public class LogIn {
+
+        private LogIn() {
+            fHashMap = new HashMap();
+        }
+    private HashMap fHashMap;
+
+        private static LogIn SINGLETON;
+        static {
+            SINGLETON = new LogIn();
+        }
+
+    @Contract("_, null -> fail")
+    public static void put(String key, Object data) {
+        if (data == null) {
+            throw new IllegalArgumentException();
+        } else {
+            SINGLETON.fHashMap.put(key, data);
+        }
+    }
+}
